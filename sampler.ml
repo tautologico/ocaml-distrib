b65ebbe5c40d015433442e3ee9e530fdf0306d76
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-03-18 19:43:41 -0300

    Moved matrix and vector operations to matvec.ml.

--- sampler.ml
+++ sampler.ml
@@ -5,6 +5,8 @@
 
 *)
 
+open Matvec
+
 type distribution =
   Uniform of float * float  (* low, high *)
 | DUniform of int * int     (* low, high *)
@@ -55,33 +57,6 @@ let r_output arr =
   print_string "c(";
   Array.iter (fun n -> print_float n; print_string ", ") arr;
   print_endline ")"
-
-(** Cholesky decomposition. Given a symmetric positive definite matrix A, 
-    return a lower-triangular matrix L such that A = LL' *)
-let cholesky a = 
-  a
-
-let matvec_mult mat vec = 
-  let n = Array.length mat in 
-  if Array.length vec != n then failwith "matvec_mult: incompatible dimensions"
-  else
-    let res = Array.create n 0.0 in 
-    for i = 1 to n do
-      res.(i) <- 0.0;
-      for j = 1 to n do 
-        res.(i) <- res.(i) +. mat.(i).(j) *. vec.(j)
-      done
-    done;
-    res
-
-(** Makes v1 <- v1 + v2 with v1 and v2 vectors *)
-let vector_displace v1 v2 = 
-  let n = Array.length v1 in 
-  if Array.length v2 != n then failwith "vector_displace: incompatible dimensions"
-  else
-    for i = 1 to n do
-      v1.(i) <- v1.(i) +. v2.(i)
-    done
 
 (** Sample from a multivariate normal distribution with mean vector 
     mu and covariance matrix sigma *)
