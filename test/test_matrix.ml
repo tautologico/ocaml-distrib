b6488a668992f0e33c3bd16bede1ade36d9e0019
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-03-19 14:40:58 -0300

    Some work on the test harness.

--- test/test_matrix.ml
+++ test/test_matrix.ml
@@ -1,12 +1,26 @@
 (*
 
-  tmatvec.ml
+  test_matrix.ml
   Tests of the matrix and vector operations. 
 
   Andrei de A. Formiga, 2013-03-17
 
 *)
 
+type ('a, 'b) test_case = 
+{
+  cname: string;
+  input: 'a;
+  output: 'b;
+}
+
+type ('a, 'b) test_batch = 
+{
+  bname: string;
+  cases: ('a, 'b) test_case list;
+}
+
+(* Tests for simple operations *)
 let m1 = Matrix.from_array 2 2
   [| 1.0; 2.0;
      1.5; 3.0 |]
@@ -14,6 +28,23 @@ let m1 = Matrix.from_array 2 2
 let m2 = Matrix.from_array 2 2
   [| 2.0; 3.0;
      2.5; 4.0 |]
+
+let m1_plus_m2 = Matrix.from_array 2 2
+  [| 3.0; 5.0;
+     4.0; 7.0 |]
+
+let m2_minus_m1 = Matrix.from_array 2 2
+  [| 1.0; 1.0;
+     1.0; 1.0 |]
+
+let m1_times_m2 = Matrix.from_array 2 2
+  [|  7.0; 11.0; 
+     10.5; 16.5 |]
+
+let m2_times_m1 = Matrix.from_array 2 2
+  [| 6.5; 13.0; 
+     8.5; 17.0 |]
+
 
 (* two symmetric positive definite matrices *)
 let sigma1 = Matrix.from_array 3 3 
@@ -40,14 +71,19 @@ let chol_sigma2 = Matrix.from_array 4 4
     0.0; 1.32288; 0.0377964; -0.037794;
     0.0;     0.0;   1.11739; -0.132962;
     0.0;     0.0;       0.0;   1.01533|]
-
-(* verify if matrix m1 is "reasonably close" to m2 *)
-let verify_matrix ?(eps=0.001) m1 m2 =
-  true 
   
 let test_cholesky () = 
   let test_case name input output = 
     let res = cholesky input in
-    verify_matrix res output in
+    Matrix.eq_eps res output in
   test_case "sigma1" sigma1 cholesky_sigma1
 
+
+let unpack_inputs x = x
+
+let test_basic_ops () = 
+  let test_add_case case = 
+    let in1, in2 = case.input in
+    let res = Matrix.add in1 in2 in
+    Matrix.eq_eps res case.output in
+  0
