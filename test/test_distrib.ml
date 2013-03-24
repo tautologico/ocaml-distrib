ea74b397e724c6148ada21a8cad30f633a67f0e5
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-03-24 16:44:04 -0300

    Added a few more multivariate normal generation tests.

--- test/test_distrib.ml
+++ test/test_distrib.ml
@@ -15,11 +15,28 @@
 
 ***)
 
+let id2 = Matrix.identity 2
+
+let sigma1 = Matrix.from_array 2 2
+  [| 1.0; 0.5; 
+     0.5; 2.0  |]
+
 let test_mvnorm2 () = 
   let mu = Matrix.vector_from_array [| 1.0; 1.0 |] in
-  let sigma = Matrix.identity 2 in
-  let mvnorms1 = Distribution.samples_mvnorm mu sigma 8 in
-  Matrix.print mvnorms1;
+  let mvnorms1 = Distribution.samples_mvnorm mu id2 90 in
+  Matrix.print_row_r mvnorms1 0;
+  Matrix.print_row_r mvnorms1 1
+
+let test_mvnorm22 () = 
+  let mu = Matrix.vector_from_array [| 0.6; 1.2 |] in
+  let sigma = Matrix.scmult id2 0.5 in
+  let mvnorms1 = Distribution.samples_mvnorm mu sigma 90 in
+  Matrix.print_row_r mvnorms1 0;
+  Matrix.print_row_r mvnorms1 1
+
+let test_mvnorm23 () = 
+  let mu = Matrix.vector_from_array [| 1.2; 0.7 |] in
+  let mvnorms1 = Distribution.samples_mvnorm mu sigma1 90 in
   Matrix.print_row_r mvnorms1 0;
   Matrix.print_row_r mvnorms1 1
 
@@ -31,6 +48,8 @@ let test_mvnorm3 () =
 
 let () = 
   test_mvnorm2 ();
+  test_mvnorm22 ();
+  test_mvnorm23 ();
   test_mvnorm3 ()
 
   
